d4e9fb024a7f94e8cb4d8a12fbfc4413639b6306
Pepe <pepe@yuilop.com>
2013-04-17 14:48:08 +0200

    Add api call

--- src/cache.erl
+++ src/cache.erl
@@ -1,17 +1,18 @@
 -module(cache).
 
 -export([
-   start_link/2, 
-   i/0, 
-   i/1, 
-   put/2, 
-   put/3, 
-   get/1, 
-   get/2, 
-   evict/0, 
-   evict/1, 
-   stop/0, 
-   stop/1
+    start_link/1,
+    start_link/2, 
+    i/0, 
+    i/1, 
+    put/2, 
+    put/3, 
+    get/1, 
+    get/2, 
+    evict/0, 
+    evict/1, 
+    stop/0, 
+    stop/1
 ]).
 
 -define(NAME, cache).
@@ -19,41 +20,41 @@
 %%
 %%
 start_link(Opts) ->
-   start_link(?NAME, Opts).
+    start_link(?NAME, Opts).
 start_link(Name, Opts) ->
-   cache_bucket:start_link(Name, Opts).
+    cache_bucket:start_link(Name, Opts).
 
 i() ->
-   i(?NAME).  
+    i(?NAME).  
 
 i(Cache) ->
-   cache_bucket:i(Cache).   
+    cache_bucket:i(Cache).   
 
 %%
 %%
 put(Key, Val) ->
-   put(?NAME, Key, Val).
+    put(?NAME, Key, Val).
 put(Cache, Key, Val) ->
-   cache_bucket:put(Cache, Key, Val).
+    cache_bucket:put(Cache, Key, Val).
 
 %%
 %%
 get(Key) ->
-   get(?NAME, Key).
+    get(?NAME, Key).
 get(Cache, Key) ->
-   cache_bucket:get(Cache, Key).
+    cache_bucket:get(Cache, Key).
 
 %%
 %%
 evict() ->
-   evict(?NAME).
+    evict(?NAME).
 evict(Cache) ->
-   cache_bucket:evict(Cache).
+    cache_bucket:evict(Cache).
 
 %%
 %%
 stop() ->
-   stop(?NAME).
+    stop(?NAME).
 stop(Cache) ->
-   cache_bucket:stop(Cache).
+    cache_bucket:stop(Cache).
 
